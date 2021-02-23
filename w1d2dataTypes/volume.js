4bec149b261671a1525c30aca279c473bc869fa0
Yogesh <yghimiray@miu.edu>
2021-02-23 14:52:28 -0600

    Update volume.js

--- w1d2dataTypes/volume.js
+++ w1d2dataTypes/volume.js
@@ -11,6 +11,13 @@ const x1 = prompt ("Enter x1 coordinate: ");
 const x2 = prompt ("Enter x2 coordinate: ");
 const y1 = prompt ("Enter y1 coordinate: ");
 const y2 = prompt ("Enter y2 coordinate: ");
-
-const distance = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+/*const xDiff =(x2-x1);
+const yDiff = (y2-y1);
+const xDiffSqr= Math.pow(xDiff,2);
+const yDiffSqr = Math.pow(yDiff,2);
+const sumSqr = xDiffSqr + yDiffSqr;
+const dist = Math.sqrt(sumSqr);
+console.log(dist);
+*/
+ const distance = Math.sqrt(Math.pow((x2-x1),2) + Math.pow( (y2-y1),2));
 console.log (distance);
