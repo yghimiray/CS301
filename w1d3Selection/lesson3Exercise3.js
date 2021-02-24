2b9b460c116e967770602a7f9fd7486f36eaa048
Yogesh <yghimiray@miu.edu>
2021-02-24 14:42:13 -0600

    Update lesson3Exercise3.js

--- w1d3Selection/lesson3Exercise3.js
+++ w1d3Selection/lesson3Exercise3.js
@@ -18,10 +18,10 @@ if(num<=0 || num >=6){
 }
 */
 switch(num){
-    case 1 : console.log ("One"); break;
-    case 2 : console.log ("Two"); break;
-    case 3 : console.log ("Three"); break;
-    case 4 : console.log ("Four"); break;
-    case 5 : console.log ("Five"); break;
+    case 1  : console.log ("One");      break;
+    case 2  : console.log ("Two");      break;
+    case 3  : console.log ("Three");    break;
+    case 4  : console.log ("Four");     break;
+    case 5  : console.log ("Five");     break;
     default : console.log ("Please enter a valid number");
 }
