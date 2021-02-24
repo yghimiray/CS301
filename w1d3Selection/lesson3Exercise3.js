1a309550ab021eb3c85ab9734b34f4844d6446ad
Yogesh <yghimiray@miu.edu>
2021-02-24 14:22:05 -0600

    Update lesson3Exercise3.js

--- w1d3Selection/lesson3Exercise3.js
+++ w1d3Selection/lesson3Exercise3.js
@@ -2,6 +2,7 @@
 
 const prompt = require ('prompt-sync')();
 const num = +prompt ("Enter a number from 1 to 5 : ");
+/*
 if(num<=0 || num >=6){
     console.log("Please enter valid number.")
 }else if(num === 1){
@@ -14,4 +15,13 @@ if(num<=0 || num >=6){
     console.log("Four")
 }else {
     console.log("Five")
+}
+*/
+switch(num){
+    case 1 : console.log ("One"); break;
+    case 2 : console.log ("Two"); break;
+    case 3 : console.log ("Three"); break;
+    case 4 : console.log ("Four"); break;
+    case 5 : console.log ("Five"); break;
+    default : console.log ("Please enter a valid number");
 }
