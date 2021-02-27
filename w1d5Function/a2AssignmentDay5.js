8af4b2e846be762ec77c8a3927e31b8c31e53971
Yogesh <yghimiray@miu.edu>
2021-02-26 19:04:22 -0600

    Update a2AssignmentDay5.js

--- w1d5Function/a2AssignmentDay5.js
+++ w1d5Function/a2AssignmentDay5.js
@@ -4,6 +4,15 @@ const radius = +prompt("Enter the radius of the circle : ");
 /*Write a function, areaOfCircle, that computes and returns area of a circle based on the
 value of input radius.
 */
+/* Defining Table
+Input: radius of a circle and height of the cylinder
+Outpur: area of circle and volume of cylinder
+Process:
+make a function of areaOfCircle which takes radius as parameter. The area of circle is PI* square 
+of radius, which returns the area 
+make a function of volumeOfCylinder which takes height as parameter. Volume of cylinder is 
+areaOfCircle * height.
+*/
 function areaOfCircle(rad){
     let area = Math.PI * rad *rad ;
     return area;
