4d6a52ff970b966b0794c11db24a8eea39c3b7dd
Yogesh <yghimiray@miu.edu>
2021-03-04 20:32:38 -0600

    Update l8arrays.js

--- w2d4Arrays/l8arrays.js
+++ w2d4Arrays/l8arrays.js
@@ -11,12 +11,12 @@ sum of firs and last elements of the array.
  */
 
 function addends(arr) {
-     const sum = arr[0] + arr[arr.length - 1];
+    const sum = arr[0] + arr[arr.length - 1];
     return sum;
 }
 
- const array = [10,15,20,25,30,35];
-console.log("The sum is :",addends(array));
+const array = [10, 15, 20, 25, 30, 35];
+console.log("The sum is :", addends(array));
 
 /* Q2. Write a function named getMiddle that returns the value of the middle element in an array. If
 the array has an even number of elements, then this function must return the average of the
@@ -44,7 +44,7 @@ function getMiddle(arr) {
     return middle;
 }
 
-console.log ("The middle element is :",getMiddle(array));
+console.log("The middle element is :", getMiddle(array));
 
 /* Q3. Write a function to rotate the elements in an array to the left by 1.
 */
@@ -54,13 +54,13 @@ console.log ("The middle element is :",getMiddle(array));
  * @param {Array} arr an array of any type
  * @returns {Array} with one left turn 
  */
-function rotateLeft(arr){
+function rotateLeft(arr) {
     const first = arr.shift();
     arr.push(first);
     return arr;
 }
 
-console.log(rotateLeft(array));
+// console.log(rotateLeft(array));
 
 /* Q4. Write a function to rotate the elements in an array to the right by 1.
 */
@@ -70,6 +70,135 @@ console.log(rotateLeft(array));
  * @param {Array} arr an array of any type.
  * @returns {Array} with one right turn 
  */
-function rotateRight(arr){
-
+function rotateRight(arr) {
+    const last = arr.pop();
+    arr.unshift(last);
+    return arr;
 }
+// console.log(rotateRight(array));
+
+/* Q5. Modify rotate functions to rotate array by n times where, n is the second parameter passed in
+the function (optional)
+*/
+
+/**
+ * Removes the last element of an array and add at first n times.
+ * @param {Array} arr an array of any types.
+ * @param {int} n rotate n times
+ * @returns {Array} with n times right turn 
+ */
+function rotateNRight(arr, n) {
+    while (n > 0) {
+        const last = arr.pop();
+        arr.unshift(last);
+        n--;
+    }
+    return arr;
+}
+console.log(rotateNRight(array,3));
+
+/*6. Write a JavaScript program that number input from the user at once as comma separated values
+and stores it into an array and do following operations
+a. Filters out negative values
+b. Maps the filtered elements to sum of its digits (optional)
+c. Reduce to get sum of all the elements (optional)
+*/
+
+const input = "1, 2, 3, 4, 5"; 
+const inputArray =  input.split(",");
+console.log(inputArray);
+
+
+/* 7.    Write a function filterRange(arr, a, b) that gets an array arr, looks for elements 
+with values higher or equal to a and lower or equal to b and return a result as an array. 
+
+describe("filterRange", function () {
+    it("tests [0, 100, 3, 6, -555], 6, 160",  function () {
+        assert.strictDeepEqual(arrays.filterRange([0, 100, 3, 6, -555], 6, 160), [100, 6]);
+    });
+});
+*/
+
+/**
+ * 
+ * @param {Array} arr an array of number.
+ * @param {int} a the lower limit
+ * @param {int} b the upper limit
+ */
+
+function filterRange(arr, a, b){
+let arr1 =[];
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] >= a && arr[i] <= b){
+        arr1.push(arr[i]);
+    }
+}
+return arr1;
+}
+
+const test = [0, 100, 3, 6, -555]
+console.log(filterRange(test,6,160));
+
+/* Q8. Write a function that takes an array of strings and returns array of palindrome strings only.
+*/
+
+/**
+ * 
+ * @param {Array} arr an array of string.
+ */
+
+function filterPalindromes(arr){
+let palinArr = [];
+for(let i = 0; i < arr.length; i++){
+    if(isPalindrome(arr[i])){
+        palinArr.push(arr[i]);
+    }
+}
+return palinArr;
+}
+
+// program to check if the string is palindrome or not
+
+/**
+ * 
+ * @param {string} str a string
+ * @returns {boolean} palindrome or not.
+ */
+
+function isPalindrome(str) {
+    const len = str.length;
+   for (let i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const testPalindrome = ["abc", "madam", "pqr", "radar"];
+console.log(filterPalindromes(testPalindrome));
+
+
+/* Q9. Do matrix addition and print result on matrix format for following:
+*/
+
+/**
+ * 
+ * @param {Array} arr1 first array of numbers
+ * @param {Array} arr2 second array of numbers
+ */
+
+function matrixAddition(arr1,arr2){
+let arr3 =[];
+for(let i = 0; i < arr1[0].length; i++){
+    for (let j = 0; j< arr1.length; j++){
+        let sum = arr1[i][j]+arr2[i][j];
+        arr3.push(sum);
+    }
+}
+return arr3;
+}
+
+const arr1 =[[0,1,2],[9,8,7]];
+const arr2 =[[6,5,4],[3,4,5]];
+console.log(matrixAddition(arr1,arr2));
